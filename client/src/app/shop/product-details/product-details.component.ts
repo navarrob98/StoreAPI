9a73eaf163aec22309b5086fd548be652f2b983f
Bryan Navarro <bryan141913@hotmail.com>
2022-04-02 17:37:09 -0700

    lazy loading and more angular

---
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from 'src/app/shared/models/product';
+import { ShopService } from '../shop.service';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.scss']
+})
+export class ProductDetailsComponent implements OnInit {
+  product: IProduct;
+
+  constructor(private shopSerive: ShopService, private activateRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.loadProduct();
+  }
+
+  loadProduct(){
+    this.shopSerive.getProduct(+this.activateRoute.snapshot.paramMap.get('id')).subscribe(product => {
+      this.product = product;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+}
